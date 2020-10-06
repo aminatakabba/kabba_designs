62930ad6e3fe7d03019ee8122e5e961fde94b536
Aminata Kabba <aminata-kabba@hotmail.com>
2020-10-06 20:10:14 +0100

    updated app

--- kabba_designs/src/App.js
+++ kabba_designs/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header'
 import { BrowserRouter  as Router, Route } from 'react-router-dom'; 
 import Banner from './Banner';
+import Info from './Info';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/">
           <Header />
           <Banner />
+          <Info />
         </Route>
       </div>
     </Router>
